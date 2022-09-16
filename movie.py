cfb2cbb58c126a47c672632acad6e90ba4602e9a
dhood4 <tactakon@gmail.com>
2022-09-16 12:55:17 -0400

    resolving merge conflict

--- movie.py
+++ movie.py
@@ -1,6 +1,3 @@
 print("Hello world! My favorite movie is Major Payne")
-<<<<<<< HEAD
 print("My favorite song is Good Kid")
-=======
 print("My favorite song is Sing About Me")
->>>>>>> favorite_song
